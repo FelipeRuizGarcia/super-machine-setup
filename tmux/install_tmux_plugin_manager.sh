ec2b10362a44e54ea895c6cc003c08612c993f60
felipe <felipe@localhost.localdomain>
2017-10-26 13:47:07 +0000

    output how to install predefined plugins, permission of exe for install script

--- tmux/install_tmux_plugin_manager.sh
+++ tmux/install_tmux_plugin_manager.sh
@@ -1,2 +1,3 @@
 cp ./tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo " To install plugins, start tmux and Hit prefix + I to fetch the plugin and source it. You should now be able to use the plugin."
