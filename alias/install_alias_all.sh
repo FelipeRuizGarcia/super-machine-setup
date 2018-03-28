72dd7745bc55fd6d7b44e47d4d65a97ba79889a9
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-03-28 01:46:04 -0600

    loading source after setup alias

--- alias/install_alias_all.sh
+++ alias/install_alias_all.sh
@@ -1,3 +1,10 @@
 #this source load all alias to the ~/.bashrc file
 cp ~/.bashrc ~/.bashrc.bkp
 cat alias_* >> ~/.bashrc
+
+source ~/.bashrc
+bashrc_sourced=$(stat -c %Y ~/.bashrc)
+
+PROMPT_COMMAND='
+    test $(stat -c %Y ~/.bashrc) -ne $bashrc_sourced && source ~/.bashrc
+'
