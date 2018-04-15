cd49dc35b225aabc90657276e78b4251e91ef3fa
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-04-15 01:44:39 -0500

    Putting alias at ~/.bash_aliases

--- alias/install_alias_all.sh
+++ alias/install_alias_all.sh
@@ -1,10 +1,2 @@
-#this source load all alias to the ~/.bashrc file
-cp ~/.bashrc ~/.bashrc.bkp
-cat alias_* >> ~/.bashrc
-
-source ~/.bashrc
-bashrc_sourced=$(stat -c %Y ~/.bashrc)
-
-PROMPT_COMMAND='
-    test $(stat -c %Y ~/.bashrc) -ne $bashrc_sourced && source ~/.bashrc
-'
+rm ~/.bash_aliases
+cat alias_* >> ~/.bash_aliases
