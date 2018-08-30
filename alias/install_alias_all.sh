fe4b035fe7b8d0969c5dca1c4671e4a210d7e89c
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-08-30 02:44:08 -0500

    Alias in a single file linked to symbolic link
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- alias/install_alias_all.sh
+++ alias/install_alias_all.sh
@@ -1,2 +1,13 @@
-rm ~/.bash_aliases
-cat alias_* >> ~/.bash_aliases
+#!/bin/bash
+
+BASEDIR=$(pwd)
+ALIASES=/home/$USER/.bash_aliases
+CURRENT_ALIASES=$BASEDIR/alias
+
+if [ -a $ALIASES ]; then
+	echo "moving prev aliases"
+	mv $ALIASES $ALIASES.bkp
+fi
+
+echo "creating aliases"
+ln -sf $CURRENT_ALIASES $ALIASES
