38aaf77cb8311365f757eb3559cb0186802fdb58
Felipe de Jesus Ruiz Garcia <felipe.de.jesus.ruiz.garcia@intel.com>
2018-12-12 11:56:14 -0600

    Improve tmux.conf installation
    symbolic links instead cp
    
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe.de.jesus.ruiz.garcia@intel.com>

--- tmux/install_tmux_plugin_manager.sh
+++ tmux/install_tmux_plugin_manager.sh
@@ -1,3 +1,5 @@
-cp ./tmux.conf ~/.tmux.conf
+rm ~/.tmux.conf
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo " To install plugins, start tmux and Hit prefix + I to fetch the plugin and source it. You should now be able to use the plugin."
