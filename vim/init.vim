cdb706d151c98bc2501f3e0b0f7b0419b8a5e4d9
Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>
2022-09-13 09:38:13 -0500

    WIP NVIM
    Signed-off-by: Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>

---
+++ vim/init.vim
@@ -0,0 +1,18 @@
+" native configs
+
+syntax enable
+
+set number
+set mouse=a
+
+set showcmd
+set showmatch
+set relativenumber
+
+" Directorio de plugins
+call plug#begin('~/.local/share/nvim/plugged')
+
+" Aquí irán los plugins a instalar
+call plug#end()
+
+" Luego de esta línea puedes agregar tus configuraciones y mappings
--- vim/install_vimrc.sh
+++ vim/install_vimrc.sh
@@ -3,7 +3,12 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# NEOVIM NEW
+rm ~/.config/nvim/init.vim
+ln -s `pwd`/init.vim ~/.config/nvim/init.vim
+
+# VIM NATIVE
 rm ~/.vimrc
 ln -s `pwd`/vimrc ~/.vimrc
 
-vim -c :PlugInstall 
+nvim -c :PlugInstall -c q!
