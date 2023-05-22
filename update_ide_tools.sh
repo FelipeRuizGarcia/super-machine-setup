d5073356d3c66caa019721b0a119d74f99475e1c
Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>
2023-05-21 18:56:22 -0600

    Update update_ide_tools
    Signed-off-by: Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>

--- update_ide_tools.sh
+++ update_ide_tools.sh
@@ -6,6 +6,9 @@ sudo dnf upgrade --refresh -y
 # update nvidia driver
 sudo nvautoinstall driver
 sudo nvautoinstall plcuda
+sudo nvautoinstall ffmpeg
+sudo nvautoinstall vidacc
+sudo nvautoinstall primec
 
 # upgrade brew
 brew upgrade
