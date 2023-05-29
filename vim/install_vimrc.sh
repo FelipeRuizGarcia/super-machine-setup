a4952e4591a4cfcd707c74ef1e52484112c7f83e
Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>
2023-05-29 08:04:10 -0600

    Improve vim dir creation
    Signed-off-by: Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>

--- vim/install_vimrc.sh
+++ vim/install_vimrc.sh
@@ -5,14 +5,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # COC List
 rm ~/.config/coc/extensions/package.json
+mkdir -p ~/.config/coc/extensions/
 ln -s `pwd`/package.json ~/.config/coc/extensions/package.json
 
 # NEOVIM LUA
 rm ~/.config/nvim/init.lua
+mkdir -p ~/.config/nvim/
 # ln -s `pwd`/init.lua ~/.config/nvim/init.lua
 
 # NEOVIM INIT VIM
 rm ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim/
 ln -s `pwd`/init.vim ~/.config/nvim/init.vim
 
 # VIM NATIVE
