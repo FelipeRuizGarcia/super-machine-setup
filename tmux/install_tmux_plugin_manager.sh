6e8bb1912f5aaf57fbd0179cd70255c29f1e092e
Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>
2023-06-13 22:41:40 -0600

    Enable automatic tmux plugins installation
    Signed-off-by: Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>

--- tmux/install_tmux_plugin_manager.sh
+++ tmux/install_tmux_plugin_manager.sh
@@ -1,5 +1,5 @@
 rm ~/.tmux.conf
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo " To install plugins, start tmux and Hit prefix + I to fetch the plugin and source it. You should now be able to use the plugin."
+# handled from tmux.conf
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
