b994ccc00d804337ec4e774865a71ef9573b58b1
Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>
2023-10-14 22:20:23 -0600

    Update setup_env script with dnf5
    Signed-off-by: Felipe Ruiz Garcia <felipe.ruiz@adapa.mx>

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-sudo dnf upgrade --refresh -y
+# minimal packages via OS;
+# Fedora and MacOs
+# Apps packages handled by brew
+# Upstream ASAP
+
+# Fedora
+dnf copr enable rpmsoftwaremanagement/dnf5-unstable ;
+sudo dnf groupinstall "Development Tools"
+sudo dnf install dnf5 dnf5-plugins # Fedora 39 ? 
+sudo dnf5 upgrade --refresh -y
 
 # require for chsh
-dnf install util-linux-user
-sudo dnf install awscli2 zsh nvim git tmux alacritty python3
+sudo dnf5 install util-linux-user
+sudo dnf5 install awscli2 zsh nvim git tmux alacritty python3 node
 
 # install omz and configure it
 # ./install_omz.sh
@@ -16,14 +25,11 @@ python3 -m pip install --user --upgrade pynvim
 # install p10k
 # configure tmux
 
-# JS / NODE INSTALLATION
-sudo dnf install nodejs -y
-
 brew tap oven-sh/bun
 brew install bun
 
 # install brew
-brew install pnpm aws-nuke aws-amplify cloud-nuke hugo  istioctl  kubernetes-cli  minikube typescript bat
+brew install pnpm aws-nuke aws-amplify cloud-nuke hugo  istioctl  kubernetes-cli  minikube typescript bat fzf
 
 # brew mac only
-# brew install --cask macs-fan-control --cask rectangle
+brew install --cask macs-fan-control --cask rectangle
