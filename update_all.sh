054c11e1b4a1788e2fe10b36fc4c3b25a28d4435
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-01-25 22:06:18 -0600

    Update update_all script with OS conditional
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- update_all.sh
+++ update_all.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# do need to grant sudo permission
+# This script is the entry point
 
-sudo echo "SMS UPDATE ALL"
-
-# os common
+# Cleaning ? Uncomment
 # brew cleanup
 # brew cleanup --prune=all
 # brew autoremove
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+    # if Fedora do need to grant sudo permission
+    sudo echo "SMS UPDATE ALL"
+
+    bash -x ./update_os_fedora.sh
+
+# WIP elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # update os
+    # update app store
+
+fi
+
+# common section
 bash -x ./update_os_tools.sh
 bash -x ./update_nvim.sh
