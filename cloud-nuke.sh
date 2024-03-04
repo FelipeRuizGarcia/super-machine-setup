638ea4d4ffdc7a2dda08367064ef65bc28d3a65f
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-03-04 12:14:21 -0600

    Move aws nuke templates to super-machine-templates
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- cloud-nuke.sh
+++
@@ -1,7 +0,0 @@
-cloud-nuke aws --region us-east-1 \
-    --region us-east-2 \
-    --region us-west-1 \
-    --region us-west-2 \
-    --resource-type apigateway,apigatewayv2,lambda,vpc,s3,iam,eip,nat-gateway,ec2,ec2-keypairs \
-    --config ./cloud-nuke-s3.yml \
-    --log-level trace --force
