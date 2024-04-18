1a6451ec52b751fc76f495a4f50f48d5ea709d7e
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-04-18 08:54:32 -0600

    update os-fedora-nvidia script
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-nvidia.sh
+++ utils/fedora/os-fedora-nvidia.sh
@@ -1,15 +1,20 @@
 # https://docs.fedoraproject.org/en-US/quick-docs/set-nvidia-as-primary-gpu-on-optimus-based-laptops/
 
 sudo akmods --force
-sudo akmods --force
 
 sudo dnf5 reinstall \
+    nvidia-modprobe \
+    nvidia-persistenced \
+    nvidia-settings \
+    xorg-x11-drv-nvidia \
+    xorg-x11-drv-nvidia-libs \
+    xorg-x11-drv-nvidia-power \
+    xorg-x11-drv-nvidia-cuda \
+    xorg-x11-drv-nvidia-cuda-libs \
+    xorg-x11-drv-nvidia-kmodsrc \
+    akmod-nvidia \
+    kmod-nvidia \
     kernel \
     kernel-core \
     kernel-modules \
-    kernel-modules-core \
-    akmod-nvidia \
-    kmod-nvidia \
-    xorg-x11-drv-nvidia \
-    xorg-x11-drv-nvidia-power \
-    xorg-x11-drv-nvidia-cuda
+    kernel-modules-core
