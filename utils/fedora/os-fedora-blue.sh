87e27ae34bd2745e4e261e77c9730ea2835da74a
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-07-10 14:58:41 -0600

    Add os-fedora-blue for audio on fedora
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-blue.sh
+++ utils/fedora/os-fedora-blue.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+# does not update the sounds devices on gnome
+sudo dnf5 reinstall -y \
+    'pulseaudio*' \
+    'alsa*' \
+    'pipewire*' \
+
+# this reload the sound devices on gnome
 sudo dnf5 reinstall -y \
     'bluez*' \
-    'pulseaudio*' \
     'gnome-bluetooth*' \
     'gnome-control-center*'
+
+sudo dnf5 -y reinstall realtek-firmware linux-firmware linux-firmware-whence
+
+sudo dnf5 reinstall 'glibc*' -y
+
+# pactl info
+# inxi -A
+
+### after reinstalling it works
+
+# sudo dnf5 install bluez bluez-utils bluez-libs bluez-cups bluez-libs-devel bluez-hid2hci bluez-hcidump bluez-obexd bluez-tools bluez-libs-devel pulseaudio-module-bluetooth --skip-unavailable
+
+# sudo dnf5 reinstall pulseaudio pulseaudio-utils pulseaudio-libs pulseaudio-module-bluetooth
+# alsa-plugins-pulseaudio
+#
+# 
+# sudo dnf5 install alsa-utils
+# sudo dnf5 reinstall pulseaudio alsa-plugins-pulseaudio alsa-lib alsa-utils
+# reboot
+# alsamixer
+#
+# pulseaudio-module-bluetooth
+# systemctl --user restart pulseaudio
