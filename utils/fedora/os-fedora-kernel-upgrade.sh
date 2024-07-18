c543046bef1a173b901b79b96dc4a65dbc3da42a
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-07-17 19:37:44 -0600

    Update upgrade kernel script
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-kernel-upgrade.sh
+++ utils/fedora/os-fedora-kernel-upgrade.sh
@@ -5,8 +5,9 @@ mokutil --sb-state
 # sudo dnf5 upgrade 'kernel*-6.9*-350.vanilla.fc40.x86_64' --releasever=40
 # sudo dnf5 install 'kernel*-6.9.8*-350.vanilla.fc40.x86_64' --releasever=40 --exclude='kernel-debuginfo*' --exclude='kernel-tools-debuginfo*'
 
-sudo dnf5 reinstall \
-    --releasever=40 \
+    # --releasever=40 \
+sudo dnf5 upgrade \
     --allow-downgrade \
     --allowerasing \
-    kernel-devel-6.9.5-350.vanilla.fc40.x86_64
+    'kernel-*6.9.*' \
+    'bpftool*6.9.*' \
