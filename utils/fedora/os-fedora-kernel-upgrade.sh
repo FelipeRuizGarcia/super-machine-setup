e657d1068aea7040326d0d87f586ba8d957d022f
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-07-20 12:43:00 -0600

    Update fedora script to upgrade the kernel
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-kernel-upgrade.sh
+++ utils/fedora/os-fedora-kernel-upgrade.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-mokutil --sb-state
+# mokutil --sb-state
 
 # sudo dnf5 upgrade 'kernel*-6.9*-350.vanilla.fc40.x86_64' --releasever=40
 # sudo dnf5 install 'kernel*-6.9.8*-350.vanilla.fc40.x86_64' --releasever=40 --exclude='kernel-debuginfo*' --exclude='kernel-tools-debuginfo*'
 
+# sudo dnf5 install rv-6.9.9-350.vanilla.fc40 --releasever=40
+
     # --releasever=40 \
-sudo dnf5 upgrade \
-    --allow-downgrade \
+    # --no-allow-downgrade \
+#sudo dnf5 upgrade \
+sudo dnf5 reinstall \
     --allowerasing \
-    'kernel-*6.9.*' \
-    'bpftool*6.9.*' \
+    --allow-downgrade \
+    --exclude='kernel-debuginfo*' \
+    --exclude='kernel-tools-debuginfo*' \
+    --exclude='libperf-debuginfo-6.9.9-350.vanilla.fc40.x86_64' \
+    --exclude='bpftool-debuginfo-6.9.9-350.vanilla.fc40.x86_64' \
+    --exclude='python3-perf-debuginfo-6.9.9-350.vanilla.fc40.x86_64' \
+    --exclude='perf-debuginfo-6.9.9-350.vanilla.fc40.x86_64' \
+    kernel \
+    bpftool \
+    kernel-core \
+    kernel-modules \
+    kernel-modules-core \
+    kernel-modules-extra \
+    kernel-modules-internal \
+    libperf \
+    perf \
+    python3-perf \
+    rtla \
+    rv
