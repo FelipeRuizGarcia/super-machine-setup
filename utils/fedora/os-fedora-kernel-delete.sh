125a7aab856b5e7c4110b7dc9104bef44a9fd13e
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-10-18 23:27:53 -0600

    Update os-fedora-kernel-delete script
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-kernel-delete.sh
+++ utils/fedora/os-fedora-kernel-delete.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # list kernel's
-rpm -q kernel
+rpm -aq kernel
 
 # delete kernel
-sudo dnf5 remove kernel
+# sudo dnf5 remove kernel
 
-# delete the no require kernel
-# kernel-debuginfo
-# kernel-debuginfo-common-x86_64-0
-# etc
+sudo dnf5 remove \
+    --exclude='kernel*6.11.4*' \
+    --exclude='kernel-*6.11.4-450.vanilla.fc41.x86_64' \
+    'kernel*' \
+    'kernel-devel*' \
+    'kernel-debuginfo*' \
+    'kernel-tools*'  \
+    'kernel-tools-debuginfo*' 
+
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
