1895cf3e7accc7ed2762bbf67e94a2fcd253058c
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-10-22 15:49:08 -0600

    Update delete-kernel sync upgrade-kernel script
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- utils/fedora/os-fedora-kernel-delete.sh
+++ utils/fedora/os-fedora-kernel-delete.sh
@@ -3,16 +3,31 @@
 # list kernel's
 rpm -aq kernel
 
-# delete kernel
-# sudo dnf5 remove kernel
+export family_kernel='6.11.4-450.vanilla';
+
+# you can not remove an "upgrade vs installed" package
+# 1. upgrade the kernel and libs to the desired kernel
+#       with the kernel-upgrade script
+# 2. 
 
 sudo dnf5 remove \
-    --exclude='kernel*6.11.4*' \
-    --exclude='kernel-*6.11.4-450.vanilla.fc41.x86_64' \
-    'kernel*' \
+    --exclude=akmod-nvidia \
+    --exclude=akmods \
+    --exclude=rpm-build \
+    --exclude='kernel*'$family_kernel'*' \
+    'kernel' \
+    'kernel-core*' \
+    'kernel-modules*' \
+    'kernel-headers*' \
+    'kernel-cross-headers*' \
+    'kernel-tools*' \
     'kernel-devel*' \
-    'kernel-debuginfo*' \
-    'kernel-tools*'  \
-    'kernel-tools-debuginfo*' 
+    'kernel-devel-matched*' \
+    'bpftool*' \
+    'perf*' \
+    'rtla*' \
+    'rv*' \
+    'libperf*' \
+    'python3-perf*' 
 
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
