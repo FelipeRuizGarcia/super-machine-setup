b02c1df6aa558ce275060307984b9d53da898276
Felipe Ruiz <felipe.ruiz@adapa.mx>
2024-11-15 15:37:48 -0600

    Rename update_os_windows
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- update_os_windows.sh
+++ update_os_windows.sh
@@ -3,6 +3,9 @@
 # This script updates the ONLY Windows OS packages
 # WINGET - WSL UPGRADE
 
-powershell.exe wsl upgrade
+powershell.exe wsl --update
 
 powershell.exe winget upgrade
+
+# restart wsl to use the latest
+# wsl --shutdown
